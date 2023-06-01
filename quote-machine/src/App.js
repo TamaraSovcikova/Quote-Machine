ad6760bd0a7ea08658fd748cea306755faa7eaa1
snaccident <tamara@sovcik.net>
2023-06-01 09:38:39 +0100

    finalizing style changes

--- quote-machine/src/App.js
+++ quote-machine/src/App.js
@@ -9,42 +9,41 @@ function App() {
   //const quotes = useSelector((state) => state.quotes); 
   return (
      <Provider store={store}>
-      <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="d-flex justify-content-center align-items-center">
         <div className="container myp-10">
           <div className="row auto justify-content-md-center">
             <div className="col">
               <div className="d-flex align-items-start">
                 <p id="quote-text" className="lead" style={{  fontSize: '30px', wordWrap: 'break-word'}}>
                 <i id="quote-icon"className="fas fa-quote-left fa-1x text-primary" style={{ marginRight: '0.8rem' }}></i>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, vero iusto et sequi nulla, iuredawd, awdapw hawjd wdjahk d aw jwahd.
+                  Lorem ipsum dolor sit amet contetur asicing elit. Nemo, vero iusto et sequi nla, iuredd, awpw had wdjk d aw jwahd.
                 </p>
               </div>
             </div>
           </div>
-
-
           <div class="row">
             <div class="col text-end">
-              <p id="author">Sam Smith</p>
+              <p id="author">- Sam Smith</p>
             </div>
           </div>
           <div class="row">
             <div class="col">
-               <div id="social-icons" class="d-flex justify-content-start">
-                <a id="instagram-link" href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" class="btn btn-info">
+               <div id="social-icons" class="flex justify-content-start">
+                <a id="instagram-link" href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" class="custom-anchor btn">
                   <i class="fab fa-instagram fa-lg text-white"></i> 
                 </a>
-                <a id="github-link" href="https://github.com/" target="_blank" rel="noopener noreferrer" class="btn btn-info">
+                <a id="github-link" href="https://github.com/" target="_blank" rel="noopener noreferrer" class="custom-anchor btn">
                   <i class="fab fa-github fa-lg text-white"></i> 
                 </a>
               </div>
             </div>
             <div class="col text-end">
-              <button id="new-quote" className="btn btn-primary">New Quote</button>
+              <button id="new-quote" class="custom-button btn">New Quote</button>
             </div>
           </div>
         </div>
       </div>
+        <p id="footer-text" className="d-flex justify-content-center align-items-center" style={{  margin: '10px'}}>by Tamara</p>
     </Provider>
   );
 }
