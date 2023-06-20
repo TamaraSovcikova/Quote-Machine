1e10c35b876b50917ed737234146b5bfe8fcd49c
snaccident <tamara@sovcik.net>
2023-06-20 13:54:24 +0100

    trying to implement checker

--- quote-machine/src/index.js
+++ quote-machine/src/index.js
@@ -1,14 +1,13 @@
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.createRoot(rootElement).render(
+createRoot(rootElement).render(
   <Provider store={store}>
     <App />
   </Provider>
